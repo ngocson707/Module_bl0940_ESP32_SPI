36d0c3f804cfff15bd6831f8dc5e517c5f8f8f57
ngocson707 <ngocson707@gmail.com>
2023-10-16 16:14:00 +0700

    The esp32 library communicates with the bl0940 module using spi
    The esp32 library communicates with the bl0940 module using spi

---
+++ BL0940_SPI/BL0940.h
@@ -0,0 +1,42 @@
+#ifndef BL0940_h
+#define BL0940_h
+
+class BL0940 {
+public:
+  BL0940();
+
+    void begin();
+    bool getCurrent(float *current);
+    bool getVoltage(float *voltage);
+    bool getActivePower(float *activePower);
+    bool getActiveEnergy(float *activeEnergy);
+    bool getPowerFactor(float *powerFactor);
+    bool getTemperature(float *temperature);
+    bool setFrequency(uint32_t Hz);
+    bool setUpdateRate(uint32_t rate);
+    bool setOverCurrentDetection(float detectionCurrent);
+    bool setCFOutputMode();
+    bool Reset();
+
+private:
+  // Add private variables and helper functions here if needed
+    const uint16_t timeout = 1000;  //Serial timeout[ms]
+    const float Vref = 1.218; //[V]
+    const float R5 = 3.3;   //[Ohm]
+    const float Rt = 2000.0;  //n:1
+    const float R8 = 20.0;  //[kOhm]
+    const float R9 = 20.0;  //[kOhm]
+    const float R10 = 20.0;  //[kOhm]
+    const float R11 = 20.0;  //[kOhm]
+    const float R12 = 20.0;  //[kOhm]
+    const float R7 = 24.0;  //[Ohm]
+    uint16_t Hz = 50;   //[Hz]
+    uint16_t updateRate = 400; //[ms]
+
+    bool transferSPI(uint8_t *sendData, uint8_t *receivedData, int dataSize) ;
+    uint8_t calculateChecksum(uint8_t *rxData, uint8_t state, uint8_t address);
+    bool writeRegister(uint8_t address, uint32_t data);
+    bool readRegister(uint8_t address, uint32_t *data);
+};
+
+#endif
---
+++ BL0940_SPI/examples/read_AC_power_values/read_AC_power_values.ino
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include "BL0940.h"
+
+BL0940 bl0940;
+
+void setup() {
+  Serial.begin(115200);
+  bl0940.begin();
+
+}
+
+void loop() {
+  float voltage;
+  bl0940.getVoltage( &voltage );
+  Serial.printf("%d [V]\n", int(voltage) );
+
+  float current;
+  bl0940.getCurrent( &current );
+  Serial.printf("%.2f [A]\n", current );
+
+  float activePower;
+  bl0940.getActivePower( &activePower );
+  Serial.printf("%.2f [W]\n", activePower );
+
+  float activeEnergy;
+  bl0940.getActiveEnergy( &activeEnergy );
+  Serial.printf("%.3f [kWh]\n", activeEnergy );
+
+  float powerFactor;
+  bl0940.getPowerFactor( &powerFactor );
+  Serial.printf("%.1f [%%]\n", powerFactor );
+
+  float temperature;
+  bl0940.getTemperature( &temperature );
+  Serial.printf("%.1f [deg C]\n", temperature );
+
+  Serial.println("");
+  delay(1000);
+}
